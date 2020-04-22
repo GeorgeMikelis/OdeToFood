c5f0424cc822f9e8d7a6c2d877b77a11c7afe0ba
George Mikelis <giorgos.mikelis@gmail.com>
2020-04-22 16:43:00 +0300

    com

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -34,6 +34,7 @@ namespace OdeToFood
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
 
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +52,15 @@ namespace OdeToFood
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    const int durationInSeconds = -1;
+                    ctx.Context.Response.Headers[HeaderNames.CacheControl] =
+                        "public,max-age=" + durationInSeconds;
+                }
+            });
 
             app.UseRouting();
 
@@ -60,6 +69,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
